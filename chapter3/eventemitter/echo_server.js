ae772c76fcb42a2e603513661b07fe7f55eaf806
huayun321 <huayun321@gmail.com>
2014-09-27 16:27:59 +0800

    用EventEmitter创建自己的事件发射器

--- chapter3/eventemitter/echo_server.js
+++ chapter3/eventemitter/echo_server.js
@@ -1,8 +1,12 @@
 var net = require('net');
 
 var server = net.createServer(function(socket) {
-    socket.on('data', function (data) {
-        socket.write(data);
+//    socket.on('data', function (data) {
+//        socket.write(data);
+//    });
+    //响应一次事件
+    socket.once('data', function (data) {
+       socket.write(data);
     });
 });
 
