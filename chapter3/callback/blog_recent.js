70639d5b94edc4f4d09a45d508c17bd1a402e166
huayun zhang <43416244@qq.com>
2014-09-27 10:25:11 +0800

    修正两个blog recent文件名颠倒的名字

--- chapter3/callback/blog_recent.js
+++ chapter3/callback/blog_recent.js
@@ -1,41 +1,30 @@
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer(function (req, res) {
-    getTitles(res);
-});
+http.createServer(function (req, res) {
+    if (req.url == '/') {
+        fs.readFile('./titles.json', function(err, data) {
+            if (err) {
+                console.log(err);
+                res.end('Server Error');
+            }
+            else {
+                var titles = JSON.parse(data.toString());
 
-server.listen(8000, "127.0.0.1");
+                fs.readFile('./template.html', function (err, data) {
+                   if (err) {
+                       console.log(err);
+                       res.end('Server Error');
+                   }
+                    else {
+                       var tmpl = data.toString();
 
-function getTitles(res) {
-    fs.readFile('./titles.json', function (err, data) {
-       if (err) {
-           hadError(err, res);
-       }
-        else {
-           getTemplate(JSON.parse(data.toString()), res);
-       }
-    });
-}
-
-function getTemplate(titles, res) {
-    fs.readFile('./template.html', function (err, data) {
-       if (err) {
-           hadError(err, res);
-       }
-        else {
-           formatHtml(titles, data.toString(), res);
-       }
-    });
-}
-
-function formatHtml(titles, tmpl, res) {
-    var html = tmpl.replace('%', titles.join('</li><li>'));
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(html);
-}
-
-function hadError (err, res) {
-    console.log(err);
-    res.end('Server Error');
-}
+                       var html = tmpl.replace('%', titles.join('</li><li>'));
+                       res.writeHead(200, {'Content-Type': 'text/html'});
+                       res.end(html);
+                   }
+                });
+            }
+        });
+    }
+}).listen(8000, "127.0.0.1");
