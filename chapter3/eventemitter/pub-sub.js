cfcafc4f94e3403c2434ae04b024107b8937876f
huayun zhang <43416244@qq.com>
2014-10-03 16:21:01 +0800

    add  process when  some client  type shutdown

--- chapter3/eventemitter/pub-sub.js
+++ chapter3/eventemitter/pub-sub.js
@@ -15,10 +15,18 @@ channel.on('join', function (id, client) {
     this.on('broadcast', this.subscriptions[id]);
 });
 
-//add listener when some client leave
+//add listener remove some client's broadcast listener
+// when some client leave
 channel.on('leave', function (id) {
     channel.removeListener('broadcast', this.subscriptions[id]);
     channel.emit('broadcast', id, id + ' has left the chat.\n');
+});
+
+//add listener remove all clients' broadcast listener
+//when some client type shutdown
+channel('shutdown', function () {
+    channel.emit('broadcast', '', "Chat has chut down.\n");
+    channel.removeAllListeners('broadcast');
 });
 
 var server = net.createServer(function (client) {
@@ -28,6 +36,10 @@ var server = net.createServer(function (client) {
     });
     client.on('data', function (data) {
         data = data.toString();
+        //when data is shutdown emit shutdown
+        if (data == "shutdown\r\n") {
+            channel.emit('shutdown');
+        }
         channel.emit('broadcast', id, data);
     });
 
@@ -35,6 +47,7 @@ var server = net.createServer(function (client) {
     client.on('close', function () {
         channel.emit('leave', id);
     });
+
 });
 
 server.listen(8888);
