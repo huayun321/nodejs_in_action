433f2fa991a3a2ceabdee053be5eec868d4ebea0
huayun321 <huayun321@gmail.com>
2014-10-17 16:35:58 +0800

    4.2.1 4.2.2  readme todo.js  post  get

---
+++ chapter4_NodeWeb/todo.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var url = require('url');
+var items = [];
+
+var server = http.createServer(function (req, res) {
+    switch (req.method) {
+        case 'POST':
+            var item = '';
+            req.setEncoding('utf8');
+            req.on('data', function(chunk) {
+               item += chunk;
+            });
+            req.on('end', function() {
+                items.push(item);
+                res.end('OK\n');
+            });
+            break;
+
+        case 'GET':
+//            items.forEach(function(item, i) {
+//                res.write(i + ') ' + item + '\n');
+//            });
+//            res.end();
+//            break;
+            //add content-length for quickly res
+            var body = items.map(function(item, i ) {
+                return i + ') ' + item;
+            }).join('\n');
+            res.setHeader('Content-Length', Buffer.byteLength(body));
+            res.setHeader('Content-type', 'text/plain; charset="utf-8"');
+            res.end(body);
+
+    }
+});
+
+server.listen(3000);
