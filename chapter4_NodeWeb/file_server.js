79f4b45af0904fe215c98dab9442f77575fae03b
huayun321 <huayun321@gmail.com>
2014-10-19 16:06:26 +0800

    4.3 readme file_server.js

---
+++ chapter4_NodeWeb/file_server.js
@@ -0,0 +1,24 @@
+/**
+ * Created by yun on 14-10-19.
+ */
+var http = require('http');
+var parse = require('url').parse;
+var join = require('path').join;
+var fs = require('fs');
+
+var root = __dirname;
+
+var server = http.createServer(function(req, res) {
+   var url = parse(req.url);
+    var path = join(root, url.pathname);
+    var stream = fs.createReadStream(path);
+//    stream.on('data', function(chuck) {
+//       res.write(chuck);
+//    });
+//    stream.on('end', function() {
+//        res.end();
+//    });
+    stream.pipe(res);
+});
+
+server.listen(3000);
