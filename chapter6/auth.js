a498940c7de9d5a19571155ea8ae7b759a2fa7b7
huayun321 <huayun321@gmail.com>
2014-11-09 13:31:42 +0800

    6.4.1 显示管理面板的中间件    认证加挂载的整体程序测试 auth.js

---
+++ chapter6/auth.js
@@ -0,0 +1,62 @@
+var connect =require('connect');
+
+function logger(req, res, next) {
+    console.log('%s %s', req.method, req.url);
+    next();
+}
+
+function hello(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('hello world');
+}
+
+function restrict(req, res, next) {
+    var authorization = req.headers.authorization;
+    if (!authorization) {
+        return next(new Error('Unauthorized'));
+    }
+
+    var parts = authorization.split(' ');
+    var scheme = parts[0];
+    var auth = new Buffer(parts[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+
+    //authenticateWithDatabase(user, pass, function(err) {
+    //    if (err) return next(err);
+    //
+    //    next();
+    //})
+    if(user == 'tobi') {
+        next();
+    } else {
+        next(new Error('user not match'));
+    }
+}
+
+function admin(req, res, next) {
+    switch (req.url) {
+        case '/':
+            res.end('try /users');
+            break;
+        case '/users':
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(['tobi', 'loki', 'jane']));
+            break;
+    }
+}
+
+function blog(req, res, next) {
+    res.end('this is blog');
+}
+
+
+
+connect()
+    .use(logger)
+    .use('/admin', restrict)
+    .use('/admin', admin)
+    .use('/blog', blog)
+    .use('/posts', blog)
+    .use(hello)
+    .listen(3000);
