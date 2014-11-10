4750427eee22273c1789de603a2e9428de65a342
huayun321 <huayun321@gmail.com>
2014-11-11 03:22:42 +0800

    6.6 使用错误处理中间件 error-all.js

---
+++ chapter6/error-all.js
@@ -0,0 +1,71 @@
+var connect = require('connect');
+
+function hello(req, res, next) {
+    if (req.url.match(/^\/hello/)) {
+        res.end('Hello World');
+    } else {
+        next();
+    }
+}
+
+var db = {
+    users: [
+        {name: 'tobi'},
+        {name: 'loki'},
+        {name: 'jane'}
+    ]
+};
+
+function users(req, res, next) {
+    var match = req.url.match(/^\/user\/(.+)/);
+    if(match) {
+        console.log(match[1]);
+        var user = db.users[0];
+        if(user) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(user));
+        } else {
+            var err= new Error('User not found');
+            err.notFound = true;
+            next(err);
+        }
+    } else {
+        next();
+    }
+};
+
+function pets(req, res, next) {
+    if(req.url.match(/^\/pet\/(.+)/)) {
+        foo();
+    } else {
+        next();
+    }
+};
+
+function errorHandler(err, req, res, next) {
+    console.log(err.stack);
+    res.setHeader('Content-Type', 'application/json');
+    if(err.notFound) {
+        res.statusCode = 404;
+        res.end(JSON.stringify({error: err.message}));
+
+    } else {
+        res.statusCode = 500;
+        res.end(JSON.stringify({error: 'Internal Server Error'}));
+    }
+};
+
+function errorPage(err, req, res, next) {
+    res.end('error page');
+}
+
+var api = connect()
+    .use(users)
+    .use(pets)
+    .use(errorHandler);
+
+var app = connect()
+    .use(hello)
+    .use('/api', api)
+    .use(errorPage)
+    .listen(3000)
